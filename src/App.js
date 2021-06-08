d8601d98bfc322fc25aa0882c47c76abf4e0cd83
Andy Hotzler <ahotzler@gmail.com>
2021-06-08 18:55:41 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* logo goes here */}
+        <h1>Welcome to your personal Beer Journal.</h1>
+        <p>
+          Beer-Journal (testing). 
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
